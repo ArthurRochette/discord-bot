b1e1a312a36dcd00d5160a0241362e2eb0e53ca4
Arthur <arthur.rochette@gmail.com>
2020-11-26 11:38:18 +0100

    Delete useless things

--- Chachi/index.js
+++ Chachi/index.js
@@ -2,95 +2,77 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 const config = require("./config.json");
 const mainVoc = config.mainvoc;
-const fs= require("fs");
+const fs = require("fs");
 
 var nbrUser = -1;
 var playing = false;
 var userInChannel = [];
 
 bot.on('ready', () => {
-    var date = new Date();
-	console.log(date.getHours()+":"+date.getMinutes()+":"+date.getSeconds()+" "+`Logged in as ${bot.user.tag}!`);
-    bot.user.setActivity("(•_•)");
-
-}); 
+	var date = new Date();
+	console.log(date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + " " + `Logged in as ${bot.user.tag}!`);
+	bot.user.setActivity("(•_•)");
+});
 
 bot.on('message', msg => {
-    if (msg.author.equals(bot.user)) return; // return si own message (seems logic no ??)
+	if (msg.author.equals(bot.user)) return; // return if own message (seems logic no ??)
 
-    if (msg.channel.type === "dm"){// if private message
-    	msg.channel.send("J'envoie rien en privé babe");
-	    msg.react("😘");
-    }
-    if(msg.content.startsWith(config.prefix)) { //mention of the bot
-        var args = msg.content.substring(config.prefix.length).split(" ");// so clear him ! WE DON'T MORE NEED THAT 
-	
-	if(args[1] === undefined){ // if the message is not identified, we don't know why ¯\_(ツ)_/¯ but in case of we don't know
-		msg.react("😏");
-		return;
-	}
-	const command = args[1].toLowerCase(); // normalization of the received message (lower case)
-	var date= new Date();
+	if (msg.channel.type === "dm") {// if private message
+		if (msg.content.startsWith("confess")) {
+			try {
+				let commandFile = require(`./commands/privateConfess.js`);
+				commandFile.run(bot, msg, args);
 
-	    console.log("message de " + msg.author.username+" a "+date.getHours()+":"+date.getMinutes()+":"+date.getSeconds());//console log
-	
-	//lets try if a .js function file is named with the lowered case command 
-	try{
-		let commandFile = require(`./commands/${command}.js`);
-		commandFile.run(bot, msg, args);
-	} catch(err){
-		console.log("/!\\"+msg.author.username + " -> Unknown :" + msg.content); //if no command file matches with the command , just add a reaction, as if the bot understood something, HAHA (evil dev laugh)
-		msg.react("🤔");
+			} catch (err) {
+				console.log("/!\\ " + msg.author.username + " -> Error confess " + msg.content);
+				console.log("err:" + err);
+				msg.react("🤒");
+			}
+		}
 	}
 
-    }
 
+	if (msg.content.startsWith(config.prefix)) { //mention of the bot
+		var args = msg.content.substring(config.prefix.length).split(" ");// so clear him ! WE DON'T MORE NEED THAT 
 
+		if (args[1] === undefined){ // if the message is not identified, we don't know why ¯\_(ツ)_/¯ but in case of we don't know
+			msg.react("😏");
+			return;
+		}
+		const command = args[1].toLowerCase(); // normalization of the received message (lower case)
+		var date = new Date();
+
+		console.log("message de " + msg.author.username + " a " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds());//console log
+
+		//lets try if a .js function file is named with the lowered case command 
+		try {
+			let commandFile = require(`./commands/${command}.js`);
+			commandFile.run(bot, msg, args);
+		} catch (err) {
+			commandFile = require(`./commands/discution.js`);
+			commandFile.run(bot, msg, args);
+		}
+
+	}
 });
 
 bot.on('end', msg => {
 	// end of play stream
 	playing = false;
 
-}); 
-/* here it is a function to count user chat activity points, leveling system 
-
-bot.on('voiceStateUpdate', (oldMember, newMember) =>{
-	let newUserChannel = newMember.voiceChannel;
-	let oldUserChannel = oldMember.voiceChannel;
-
-	if(oldUserChannel ==undefined && newUserChannel !== undefined){
-				
-		if(newMember.id === "#ENTER OWNER ID" && playing === false){ // this feature is the dogo system, the bot follow it's owner through voice channel (wuaf) 🐶
-			newMember.voiceChannel.join();
-		}
-		
-		//User joins a voice channel
-	} else if(newUserChannel === undefined){
-		var channelsArray = bot.voiceConnections.get("#ENTER VOICE ID CHANNEL");
-		console.log(" " + channelsArray);
-		for(let i = 0;i<channelsArray.length; i++){
-			console.log(channelsArray[i]);
-		}
-		
-		//user leave
-	} else {
-		if(newMember.id === "#ENTER OWNER ID" && playing === false){
-			newMember.voiceChannel.join();
-		
-		}
-		//user move 
-	}
 });
-*/
-bot.login(config.token);
+
 
 bot.on('disconnect', function () {
-	var date =  new Date();
-    console.log(date.getHours()+":"+date.getMinutes()+":"+date.getSeconds()+" " + "bot is offline ! ! ! ! !");//time
+	var date = new Date();
+	console.log(date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + " " + "bot is offline ! ! ! ! !");//time
 });
 
 bot.on('reconnecting', function () {
 	var date = new Date();
-    console.log(date.getHours()+":"+date.getMinutes()+":"+date.getSeconds()+" "+" trying to reconnect !!");
+	console.log(date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + " " + " trying to reconnect !!");
 });
+
+
+
+bot.login(config.token);
