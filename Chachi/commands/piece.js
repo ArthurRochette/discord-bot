75c55fdc81fa0d4fe65b638805af250e50e3af00
ArthurSenpaii <42816955+ArthurSenpaii@users.noreply.github.com>
2021-01-27 20:55:44 +0100

    Update piece.js

--- Chachi/commands/piece.js
+++ Chachi/commands/piece.js
@@ -3,9 +3,9 @@ exports.run = (bot, msg, args) => {
 
     let face = Math.floor(Math.random() * 2)+1;
     if(face == 1){
-        msg.channel.send("<:coin:788358806504603658> C'est face !");
+        msg.channel.send("<:coin:788358806504603658> C'est head !");
     }else {
-        msg.channel.send("<:coin:788358806504603658> C'est pile !");
+        msg.channel.send("<:coin:788358806504603658> C'est face !");
     }
     
 
